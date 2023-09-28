784c30c589adf8e35e0d66ef5dca873af4864902
Victor Kreniski <victor.kreniski@airapps.com>
2023-09-28 16:19:04 -0300

    IOSTUR-893: Removed Support from MacOS Catalyst

--- Package.swift
+++ Package.swift
@@ -6,8 +6,7 @@ import PackageDescription
 let package = Package(
     name: "AirSettingsUI",
     platforms: [
-        .iOS(.v15),
-        .macCatalyst(.v15)
+        .iOS(.v15)
     ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
